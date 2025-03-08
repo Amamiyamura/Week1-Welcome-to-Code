217d086231044c47542d0d6b08cee9985673526a
Amamiyamura <amarimiyamura@gmail.com>
2025-03-08 06:30:05 +0000

    Nyerahin tugas

--- quiz/part1/soal1.js
+++ quiz/part1/soal1.js
@@ -1,6 +1,5 @@
-// Conditional Statement | Part 1
-
-// ## Soal 1:
+// Soal 1:
+// js
 // Kamu diminta untuk memprogram suatu game sederhana, Proxytia namanya. 
 // Untuk memulai game itu diperlukan 2 variabel (untuk sekarang), 
 // yaitu nama dan peran. Variabel peran harus memiliki isi data, 
@@ -19,32 +18,40 @@
 //tapi tanpa backtick juga ga masalah sih yg penting output sesuai
 
 // algoritma
-//isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
+// isi algoritma mu disini (AWAS KALO GA DI ISI!!!!)
+// Selamat Datang di Game Proxytia
+// Inisialisasi nama dan peran
+// jika nama kosong maka tampilkan nama wajib diisi
+//     jika nama terisi maka tampilkan Halo $(nama) , Selamat Datang di Game Proxytia
+// jika peran kosong maka tampilkan Pilih peranmu untuk memulai game
+//     jika peran === Ksatria maka tampilkan halo Ksatria $(nama) , kamu dapat menyerang dengan senjatamu!
+//     dan jika peran === Tabib maka tampilkan halo Tabib $(nama) , kamu akan membantu temanmu yang terluka!
+//     dan jika peran === Penyihir maka tampilkan halo Penyihir $(nama) , ciptakan keajaiban yang membantu kemenanganmu!
+//     jika peran tidak ada maka tampilkan kamu jadi bot aja ya, peran yang kamu pilih ga ada
+// Program selesai
+
 
 
 let nama = "", peran = "";
 
+
 //code disini gunakan console.log untuk outputnya
-
-```
-
-## Soal 2
-```
-// Kamu akan diberikan sebuah tanggal dalam tiga variabel, yaitu hari, bulan, dan tahun. Disini kamu diminta untuk membuat format tanggal. Misal tanggal yang diberikan adalah hari 1, bulan 5, dan tahun 1945. Maka, output yang harus kamu proses adalah menjadi 1 Mei 1945.
-
-// Gunakan switch case untuk kasus ini!
-
-// Contoh:
-
-// let hari = 21; let bulan = 1; let tahun = 1945;
-
-// Maka hasil yang akan tampil di console adalah: '21 Januari 1945';
-
-//tips gunakan keyword ini di google "conditional switch case javascript"
-//dan mulailah membaca dan mencontek di stackoverflow xixixi
-
-let tanggal = 12; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
-let bulan = "februari"; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
-let tahun = 2001; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
-
-//code switch case kamu disini
+console.log(" ========= Proxytia ========= ");
+nama = "Ackerman";
+if (nama === "") {
+    console.log("Nama wajib diisi!!");
+} else {
+    console.log(`Halo ${nama} , Selamat datang di game Proxytia`);
+}
+peran = "Penyihir";
+if (peran === "") {
+    console.log("Pilih Peranmu untuk memulai game")
+} else if (peran === "Ksatria") {
+    console.log(`halo Ksatria ${nama} , kamu dapat menyerang dengan senjatamu!`)
+} else if (peran === "Tabib") {
+        console.log(`halo Tabib ${nama} , kamu akan membantu temanmu yang terluka!`)
+} else if (peran === "Penyihir") {
+        console.log(`halo Penyihir ${nama} , ciptakan keajaiban yang membantu kemenanganmu!`)
+} else {
+        console.log('kamu jadi bot aja ya, peran yang kamu pilih ga ada')
+};
